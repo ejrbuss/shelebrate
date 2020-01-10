dc36e920e10ad1496113e0fb9e3504d0d80b8f82
ejrbuss <ejrbuss@gmail.com>
2020-01-09 20:39:21 -0800

    Project setup

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp ./target/*.jar net.ejrbuss.shelebrate.Main
---
+++ src/main/java/net/ejrbuss/shelebrate/Main.java
@@ -0,0 +1,13 @@
+package net.ejrbuss.shelebrate;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
